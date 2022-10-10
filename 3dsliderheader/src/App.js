5d35de9382d3734f676794aaabe8ccbf4223093b
Paloma Passos <31938621+passospaloma@users.noreply.github.com>
2022-10-10 16:50:01 -0700

    Add files via upload

---
+++ 3dsliderheader/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+import './global.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+
+    </div>
+  );
+}
+

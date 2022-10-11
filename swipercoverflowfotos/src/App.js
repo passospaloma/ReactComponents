d3c44764e97f07956c7b3df11705dc20aca18d38
Paloma Passos <31938621+passospaloma@users.noreply.github.com>
2022-10-10 17:09:59 -0700

    Add files via upload

---
+++ swipercoverflowfotos/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './global.css';
+import Fotos from './Fotos';
+
+export default function App() {
+  
+  return (
+    <div className="App">
+      
+      <Fotos />
+
+    </div>
+  );
+}
+
+
